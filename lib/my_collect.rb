a0ab6b576e01449c6743ed8d7df615edba22e9a2
morgvanny <morgvanny@gmail.com>
2015-10-29 06:22:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array = [])
+  length = array.length
+  i = 0
+  new_array = []
+  while length > 0
+    new_array << yield(array[i])
+    length -= 1
+    i += 1
+  end
+  new_array
+end
 
-
+my_collect do |i|
+  array[i].split(" ").first
+end
