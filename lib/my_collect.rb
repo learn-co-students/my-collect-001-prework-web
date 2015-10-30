e4fe1569fb781e0fc818ce47641720bc8063c2b3
morgvanny <morgvanny@gmail.com>
2015-10-30 05:05:21 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,8 @@
 def my_collect(array = [])
-  length = array.length
   i = 0
   new_array = []
-  while length > 0
+  while array[i]
     new_array << yield(array[i])
-    length -= 1
     i += 1
   end
   new_array
