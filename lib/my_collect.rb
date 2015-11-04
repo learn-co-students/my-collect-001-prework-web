e9c38e15e89c8b84b548f1c01ff2690b3eb51723
simque <simon.duncanson@gmail.com>
2015-11-04 14:09:49 -0500

    Done with reservations

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(array)
+  new_array = []
+  counter = 0
+  while counter <= array.length - 1
+    name = array[counter].split.first
+    yield(name)
+    new_array << name.upcase
+    counter += 1
+  end
+  new_array
+end
 
