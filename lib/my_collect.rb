d32dc514e333606bd1b2902e6c26c7c102ba2554
shmulim <shmuel.markel@gmail.com>
2015-11-20 12:34:35 -0800

    Done

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(arr)
+  i = 0
+  return_val = []
+  while i < arr.length
+    yield_return_val = yield(arr[i])
+    return_val << yield_return_val
+    i+=1
+  end
+  return_val
+end
 
