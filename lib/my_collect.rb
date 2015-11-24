9b1779893bbf67e54685ec2d95c6d69781299285
febbraiod <donedesign1@gmail.com>
2015-11-24 15:22:21 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+  new_collection = []
 
+  idx = 0
+  while idx < array.length
+    j = yield array[idx]
+    new_collection << j
+    idx += 1
+  end
+
+  new_collection
+end
 
