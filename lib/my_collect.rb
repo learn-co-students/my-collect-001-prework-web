25610f4be76ac20b1f243a71f47bf851a9f7925a
Munish Chaturvedi <munishchaturvedi1@gmail.com>
2015-11-24 20:46:34 -0600

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(collection)
+  collected = []
+  i = 0
+    until i >= (collection.length)
+      collected.push(yield(collection[i]))
+      i+=1
+    end
+    return collected
+    return collection
+end
 
-
+my_collect(['ruby', 'javascript', 'python', 'objective-c']){ |language|
+ language
+}
