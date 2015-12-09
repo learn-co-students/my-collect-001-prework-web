7619d44b03fd37953ee4d0d248a9bfecbad88150
Adam King <adam.king0126@gmail.com>
2015-12-09 01:29:26 +0000

    wrote my_collect

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array) # put argument(s) here)
+  count = 0
+  new_array = []
+  until count == array.length do
+    new_element = yield array[count]
+    new_array << new_element
+    count += 1
+  end
+  new_array
+end
