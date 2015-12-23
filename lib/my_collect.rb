86891f3ad07ea632b84200ddfdf78dfd0385c59f
chintas1 <chintas1@tcnj.edu>
2015-12-23 23:54:41 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   count = array.size
   result = []
   while (count > 0)
-    result << yield array[count-1]
+    result.unshift(yield(array[count-1]))
     count -= 1
   end
   result
