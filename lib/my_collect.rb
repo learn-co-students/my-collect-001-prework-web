1ad792589b0d362b1d2caed88b3acbda06f0d4bf
franco84 <franco84@gmail.com>
2015-12-30 00:55:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+  newarray = []
+  counter = 0
+  while counter < array.length
+    arr_item = yield array[counter]
+    newarray << arr_item
+    counter += 1
+  end
+    newarray
+end
