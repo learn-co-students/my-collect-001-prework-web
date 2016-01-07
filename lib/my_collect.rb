dcecf7717af9f514696cae85a5ad028aeb3357aa
WilliamAG <william.a.gough@gmail.com>
2016-01-07 01:07:30 -0500

    done

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,19 @@
-def my_collect(input) 
-  output = []
-  input.each do |x|
-    v = yield x
-    output.push(v)
+# def my_collect(input) 
+#   output = []
+#   input.each do |x|
+#     v = yield x
+#     output.push(v)
+#     binding.pry 
+#   end
+#   output
+# end
+
+def my_collect(array)
+  new_array = []
+  num = array.length
+  num.times do |x|
+    v = yield array[x] 
+    new_array << v 
   end
-  output
+  new_array  
 end
