52f7ad74a54bd2f5e904595c6b193ec9648f65d4
SaiedAbbasi <saiedabbasijr@gmail.com>
2016-01-16 22:25:02 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(array)
+  a2 = []
+  if block_given?
+    i = 0
+    while i < array.length
+    a2 << yield(array[i])
+    i += 1
+    end
+  else
+  array
+  end
+  a2
+end
 
-
+#array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+#my_collect(array) do |name|
+#  name.split(" ").first
+#end
