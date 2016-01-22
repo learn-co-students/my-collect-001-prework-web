94d4c9ba22aab89e83884736cd9b0ad7e980a60b
chrism81 <chrism81@msn.com>
2016-01-22 11:18:46 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(names)
+  if names.empty? == false
+    i=0
+    newnames=[]
+    while i<names.length
+      k= yield (names[i])
+      newnames<<k
+      i+=1
+    end
+    newnames
+  end
+  
+end
