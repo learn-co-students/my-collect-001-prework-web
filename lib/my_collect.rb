222da0a5c9d8620abd49b273a308099326411d00
ericajayne <ericajayne.walsh@gmail.com>
2016-01-23 23:55:21 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,33 @@
 
+array =["blue birds", "brown trees", "great friends"]
 
+def my_collect (array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array << yield(array[i])
+    i = i + 1
+  end
+  new_array
+end
+
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+
+=begin
+def my_collect (array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array << yield(array[i])
+    i = i + 1
+  end
+  new_array
+end
+
+my_collect(array) do |name|
+  name.split(" ").first
+end
+=end
