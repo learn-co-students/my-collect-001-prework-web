71615bbbdc1f6b2f8f3d226015e9f7d9784fd0dd
Brian Bier <briansbier@gmail.com>
2016-02-05 22:05:39 -0500

    Solution complete

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(array)
+        array.clone
+    i = 0
+    new_array = Array.new
+    while i < array.length
+      yield array[i]
+      new_array << array[i].upcase
+      i += 1
+    end
+    new_array
+end
 
-
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(languages) do |language|
+      language.upcase
+    end
