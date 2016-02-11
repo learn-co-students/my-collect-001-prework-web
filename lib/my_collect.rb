b5914bdd71fa23421104b08803d06916fe3017eb
jd2rogers2 <jd2rogers2@gmail.com>
2016-02-11 07:01:21 -0500

    donzo

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+# use while to mimic collect
 
+def my_collect(array)
+  counter = 0
+  new_array = []
+  while counter < array.count
+    new_array << yield(array[counter])
+    counter += 1
+  end
+  return new_array
+end
 
