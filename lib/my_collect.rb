1da0c51a06d87948c89c5a214181feb16061d3f2
qlaire <cdranginis@gmail.com>
2016-02-15 09:22:47 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(array)
   i = 0
   new_array = []
   while i < array.length
-    yield array[i]
-    new_array << array[i].upcase
+    ret = yield array[i]
+    new_array << ret
     i += 1
   end
   new_array
