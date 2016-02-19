c5d5561b19a7c20d4b9ddd343af24f6bceeda9fe
kazushige Miyao <yura.soverigns@gmail.com>
2016-02-19 18:02:00 +0900

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+require 'pry'
+def my_collect(array)
+	i = 0
+	my_collection = [] 
+	while i < array.size  do
+		my_collection[i] = yield array[i]
+		i += 1
+	end
+	my_collection
+end
 
-
+my_collect(1) do |x| 
+end
