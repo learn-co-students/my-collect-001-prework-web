5fa1bad19b5babe4858659e077f55aa46ede185f
blochbr <blochbr@gmail.com>
2016-02-20 04:05:04 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(collection)
+  counter = 0
+  new_array = []
+  while counter != collection.length
+    yield(new_array[counter] = collection[counter].upcase)
+    counter += 1
+    end
+  return new_array
+  end
 
 
