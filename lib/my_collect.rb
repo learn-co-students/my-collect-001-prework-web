0b80717455099e8d164653cd64dda134f37ea24d
jonalexander <jonathandalexander@gmail.com>
2016-02-23 13:14:22 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(array)
+
+	i = 0
+	modArray = []
+
+	while i < array.length do
+		yield array[i]
+		modArray << array[i].upcase
+		i += 1
+	end
+
+return modArray
 
 
+end
+
+
+# take in one array
+# iterate over each item in the array
+# return new array of modified values
+# don't use .collection/.map/.each
