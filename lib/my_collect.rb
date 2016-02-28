4ea0c847b8e3eebe8e811d3d7a017f81002881df
czarnega <c.zarnegar@gmail.com>
2016-02-28 11:24:46 -0800

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+  newArr = []
+  counter = 0
+  while counter < array.length do 
+    newArr << yield(array[counter])
+    counter = counter + 1
+  end
+  return newArr
+
+end
 
 
