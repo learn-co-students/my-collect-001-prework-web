2df26fb575b29cf8f460cbc53cbbbb6f755d8f0b
einarstensson <einar.stensson@gmail.com>
2016-02-29 13:11:58 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+  if block_given?
+    modified_array = []
 
+    array.each do |item|
+      modified_array << yield(item)
+    end
 
+    modified_array
+  else
+    nil
+  end
+end
