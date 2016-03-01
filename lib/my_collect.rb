04f32824dcc34d34ba857c4fd8a733a7c0daaa55
jwrb27 <jwrb27@gmail.com>
2016-02-29 21:48:43 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(students)
+  i = 0
+  first_names = []
+  while i < students.length
+    name = yield (students[i])
+    i += 1
+    first_names << name
+  end
+  first_names
+end
 
