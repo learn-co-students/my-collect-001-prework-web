3b63e33b9a1818e65154a55630e304db66a2d754
Wen Tran <wentran@Wen-Trans-MacBook-Pro.local>
2016-02-29 22:19:29 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  new_array = []
+  if block_given?
+    i = 0
+    array.collect do |item|
+      new_array << "#{item}".upcase
+      i = i + 1
+    end
+    new_array
+  else
+    "Hey! No block"
+  end
+end
 
 
