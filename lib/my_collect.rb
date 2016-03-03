532e0fbdd32824d7c2aaea314d53f06c5a36c9f8
JennyShalai <jenny.shalai@gmail.com>
2016-03-03 12:27:21 -0500

    can not commit though learn app

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(array)
+  
+    i = 0
+    tempArray = []
+    while i < array.length
+      tempArray.push(array[i])
+      yield(array[i])
+      i = i + 1
+    end
+    puts tempArray
+  
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+my_collect(array) do |name|
+  name.split(" ").first
+end
