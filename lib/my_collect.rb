a1745f5d77c768b58630f38b99cbcf365aa1e824
wengong <wentoshare@gmail.com>
2016-03-12 22:15:13 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(abc)
+  i=0
+  while i<abc.size do
+    yield abc[i]
+    i+=1
+  end
+  abc.map(&:upcase)
+end
 
 
+
+my_collect(array) { |name| name.split(" ").first}
