a6ef2e1b3be36e99179d8cd36907effd663915fd
Reed Dunkle <reeddunkle@gmail.com>
2016-03-13 18:39:33 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(array)
+  i = 0
+  output_array = []
+  while i < array.length
+    # How do I access what the block returns to create a new collection?
+    output_array << yield(array[i])
+    i += 1
+  end
+  output_array
+end
 
