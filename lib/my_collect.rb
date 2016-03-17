66d4908c47ee0bc1f038a16275fc97cf0becf9c9
kallaugher <alicekallaugher@gmail.com>
2016-03-16 22:34:20 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    yield(array[i])
+    new_array.push(yield(array[i]))
+    i += 1
+  end
+  return new_array
+end
 
-
+my_collect(["hello", "hi", "hello"]) do |item|
+  puts item.upcase
+end
