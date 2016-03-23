e19846c346ab10caea1ff8dd0490fc03019ac1c0
colbyschulz <colbyschulz@gmail.com>
2016-03-23 17:33:07 -0500

    my_collect

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
 
-
+def my_collect(array)
+    i = 0
+    collector_array = []
+        while i < array.length
+            collector_array << yield(array[i])
+            i += 1
+        end
+        collector_array
+end
