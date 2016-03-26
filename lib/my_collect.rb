00ed30ceb1d47ede559cebc4e60719201384ffba
chriswebb09 <chris.webb5249@gmail.com>
2016-03-26 02:11:39 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(languages)
+  if block_given?
+    i = 0
+    new_array = Array.new
+    while i < languages.length
+      yield languages[i]
+        new_array << yield(languages[i])
+        i = i + 1
+    end
+    new_array
+  else
+    languages
+  end
+end
