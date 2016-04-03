2c71a98c518bd3d2548c015f98b6a3db60c56286
abcheromar <abcheromar@gmail.com>
2016-04-02 20:27:05 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+
+ return_array = [] 
+ i = 0
+
+ while i < array.size
+    return_array << yield(array[i]) if block_given?
+    i += 1
+ end
+
+ return_array
 
 
+end
+
+
+
+
+ 
