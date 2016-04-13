df0625bcd260a36525f0d43d140daa029b9df373
blessedsoy <unmigong@gmail.com>
2016-04-12 21:16:00 -0400

    My Collect (yield)

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
 
+def my_collect (students)
+  i = 0;
+  new = []
+  while i < students.length 
+  
+    new.push(yield students[i]) 
+    i += 1
+  end
+  
+  new
+end
+
+
+array = []
+
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
