98ff3fd193e371374f7fbf86cd2835ceb7d29884
ozPop <ozziepopovas@gmail.com>
2016-04-18 14:15:39 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,7 @@ def my_collect(arr)
   i = 0
   newArr = []
   while i < arr.length
-    yield temp = arr[i]
-          newArr.push(temp.upcase)
+    newArr.push( yield arr[i] )
     i += 1
   end
   newArr
