6fa06caa9aff545d18b2c4e5d137e6a179794c3c
mitchwd <mitch@mitchwd.com>
2016-04-20 16:39:16 +0930

    Working, needs refactoring?

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(collection)
+  if collection.length > 0
+    x = 0
+    new_collection = []
+    while x < collection.length
+      new_collection << yield(collection[x])
+        x += 1
+    end
+    new_collection
+  else
+    return
+  end
+end
