5ed8d6e7b543aaa86ab83ee0ba6f4621c6195a97
thedailyephemera <thedailyephemera@gmail.com>
2016-04-24 16:50:00 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(array)
+	counter = 0
+	new_collection = []
+	while counter < array.count do
+		yield array[counter].split(" ").first
+		new_collection << array[counter].upcase
+		counter += 1
+	end
+	return new_collection
+end
 
