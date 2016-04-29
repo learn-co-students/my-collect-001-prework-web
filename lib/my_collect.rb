dd80305f62f74b597d8c9cf5ca1e9fcc5f485aff
Fidel Severino <fidelscodes@gmail.com>
2016-04-29 14:52:51 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+  i = 0
+  arr = []
+  while i < array.length
+    arr << yield(array[i])
+    i += 1
+  end
+  arr
+end
 
+# a bit cleaner implementation:
 
+# def my_collect(array)
+#   arr = []
+#   array.each do |elem|
+#     arr << yield(elem)
+#   end
+#   arr
+# end
