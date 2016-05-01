93c76e1d268756be54975e438aa749a3a2778bbc
chugachski <natesmith.copper@gmail.com>
2016-05-01 14:04:33 -0800

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  container = []
+  counter = 0
+  while counter < array.length
+    container << yield(array[counter])
+    counter += 1
+  end
+  container
+end
 
-
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(array) do |name|
+#   name.split(" ").first
+# end
