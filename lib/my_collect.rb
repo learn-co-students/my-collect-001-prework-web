cf219e9f4c9d6d597c6c63ecc12540989483bd84
Max E <etiemaxo@gmail.com>
2016-05-10 18:26:00 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  if block_given?
+    new_collection = []
+    i = 0
+    while i < array.length
+      yield(array[i])
+      new_collection << yield(array[i])
+      i = i + 1
+    end
+    array
+  end
+  new_collection
+end
 
 
