f5d94f9a05aa2c0d7375b8e49f17c3065b209776
Kellye Greene <kellye@gust.com>
2016-05-18 10:36:26 -0400

    Kinda getting it

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(names)
+  if block_given?
+    name = 0
+    first_names = []
+    while name < names.length
+      first_names << yield(names[name].upcase)
+      name += 1
+    end
+  end
+  first_names
+end
