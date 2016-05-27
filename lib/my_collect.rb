83ecfcf85999d95fe937b4c04eee35a80478dac3
conuska <onuskacc@gmail.com>
2016-05-27 14:17:10 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+  new_array = []
+  i = 0
+  while i < array.length
+    changed_item = yield array[i]
+    new_array << changed_item
+    i+= 1
+  end
+  return new_array
+end
