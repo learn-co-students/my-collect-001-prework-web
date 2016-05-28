98907f7c6ea3768a2390301e9df2353fb96a3672
gnwankwo <gnwankwo03@gmail.com>
2016-05-28 05:45:58 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  counter=0
+  newArray=[]
+  current_array=array.clone
+  while (counter< array.length)
+    
+     out= yield  array[counter]
+   newArray.push(out)
+    counter+=1
+   
+  end
+  return newArray
+end
