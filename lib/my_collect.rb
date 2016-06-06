8e686620c7651c942b4373204c6fa37d36f9c928
debashish2549 <debashish.deep74@qmail.cuny.edu>
2016-06-06 01:24:48 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect (array)
+  i = 0
+  array_first = []
+  while (i<array.length) do
+    name = yield (array[i])
+    array_first << name
+    i+=1
+  end
+  array_first
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect (array) do |name|
+  name.split(" ").first
+end
