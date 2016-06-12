52903ce0b40eb72a8645a1a145d2c50533bd3ba6
calebcox725 <cacox25+1@gmail.com>
2016-06-12 15:55:36 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,12 @@
 def my_collect(collection)
+  new_collection = []
+  
   i = 0
   while i < collection.length
-    yield collection[i]
+    new_collection[i] = yield collection[i]
     i += 1
   end
-  collection
+
+  return new_collection
 end
 
