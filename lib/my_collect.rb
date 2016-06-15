f255bfdb2bad0b7658f1e678fbcf3a979035173e
Rui Sun <rickiebosun@gmail.com>
2016-06-15 04:57:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collections)
+  i = 0
+  new_collections = []
+  while i < collections.length
+    new_collections << yield(collections[i])
+    i += 1
+  end
+  new_collections
+end
