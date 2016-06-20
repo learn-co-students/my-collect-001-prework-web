43afdea9466a2af1052828374ddf498184cec862
Tami <catquarks@gmail.com>
2016-06-19 20:44:23 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+# require 'pry'
+def my_collect(array)
+
+	collection = []
+	i = 0
+	while i < array.length
+		value = yield array[i]
+		collection.push(value) #bahhhh
+		i += 1
+	end
+	return collection
+end
+
+names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
 
+my_collect(names) { |name| name.split(" ").first }
