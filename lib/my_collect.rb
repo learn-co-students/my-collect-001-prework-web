cc3845e2cac902af5f9901e37f0346e56cdd8582
Schyler Hosler <schylerhosler@gmail.com>
2016-06-23 19:02:09 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(collection)
+  i = 0
+  collection2 = []
+  while i < collection.size
+    yield collection[i]
+      item = collection[i].upcase
+      collection2 << item
+      i += 1
+  end
+  return collection2
+end
