f39cce95e9fe7f9935bd795df246a73051517868
Akhmet Safarov <akhmetsafarov1989@icloud.com>
2016-06-27 01:01:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(students)
+    i = 0
+    names = []
+    while i < students.length
+      yield students[i]
+      names << students[i]
+      i+=1
+    end
+    return names
+end
