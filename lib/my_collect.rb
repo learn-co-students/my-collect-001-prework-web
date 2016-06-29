aa4034763aee9baa27cc2789e62f372cfc6a3f73
Parmanand Sahadeo <parmsahadeo530@gmail.com>
2016-06-29 01:37:21 +0000

    Wrote #my_collect using yield

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(collection)
-  temp_array = []
+  temp_arr = []
   i = 0
   while(i < collection.size)
-    temp_array[i] = yield collection[i]
+    temp_arr[i] = yield collection[i]
     i = i + 1
   end
-  temp_array
+  temp_arr
 end
