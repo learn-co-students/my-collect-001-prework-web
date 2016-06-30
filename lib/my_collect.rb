aac7fd03b2d8bda38b0fa82a0bf9d41a871da079
John Molina <jrmolin90@gmail.com>
2016-06-30 15:07:24 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,9 @@
 def my_collect(array)
-  if block_given?
-    new_arr = []
-    i = 0
-    while i < array.length do
-      new_arr[i] = yield array[i]
-      i += 1
-    end
-    new_arr
-  else
-    return nil
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
   end
+  collect
 end
