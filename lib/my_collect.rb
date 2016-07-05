16535e87d6452661395b89089fc1e30056187957
mo6709 <mo6709@gmail.com>
2016-07-05 11:45:19 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+require 'pry'
 
+def my_collect(array)
+	#code for the collect method
+	counter = 0 
+	my_new_collection = []
 
+	while counter < array.length
+		j = yield array[counter]
+		my_new_collection << j
+		counter += 1
+	end
+
+	my_new_collection
+end
+
+#binding.pry
