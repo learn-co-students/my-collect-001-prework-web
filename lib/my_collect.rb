f34eb5c458181d3a4260e84e80608fa664d0f635
daniellasheva <daniellasheva@gmail.com>
2016-07-13 15:03:48 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect (array)
+    i = 0
+    collect= []
+    while i <array.length
+        collect.push yield array[i]
+        i = i +1
+    end
+    collect
+end
 
 
+# Why didn't this work when I wrote "collect << yield array[i]?
