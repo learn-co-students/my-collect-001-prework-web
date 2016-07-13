11f236e8c174b095f986476ad041f9f5dfcc8b46
Robert Domingo <moondig@gmail.com>
2016-07-13 03:32:48 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(collection)
+  new_collection = []
+  i = 0
+  while i < collection.length
+    new_element = yield collection[i]
+    new_collection << new_element
+    i += 1
+  end
+  new_collection
+end
 
-
+my_collect(["a", "b", "c"]) do |i|
+  i
+end
