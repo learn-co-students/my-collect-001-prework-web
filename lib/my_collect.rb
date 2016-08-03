e42f70611ca4455e6898b127f0fdddcb1629b809
Zack Adams <adams.z.d@gmail.com>
2016-08-03 15:40:17 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+  x = 0
+  new_array = []
+  while x < array.length
+    stuff = yield(array[x])
+    new_array.push(stuff)
+      x += 1
+    end
+    return new_array
+end
