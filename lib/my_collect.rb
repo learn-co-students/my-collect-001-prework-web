7aded0e13e42da5eb3d86ba9bbb15755ed1476f8
junklpm <lance.p.miller@gmail.com>
2016-08-16 22:42:18 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,10 @@
 def my_collect(array)
   i = 0
   new_array = []
-  while new_array < array.length
-    collect << yield(array[i])
-    
+  while i < array.length
+    new_array << yield(array[i])
+    i+=1
+ end
+ new_array
 end
 
