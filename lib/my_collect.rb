97b9462221461b31bfd6795eb8ac77a349109d05
Michael Wiggins <michael.wiggins@me.com>
2016-08-18 21:04:00 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array)
+  counter = 0
+  myEmptyArray = []
+  while counter < array.length
+    myEmptyArray << (yield(array[counter]))
+    counter += 1
+  end
+  myEmptyArray
+end
