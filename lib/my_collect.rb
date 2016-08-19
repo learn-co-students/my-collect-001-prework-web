e91d9600e18c61ef4738459f261d362adc5a25b5
Michael Wiggins <michael.wiggins@me.com>
2016-08-19 13:32:16 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,12 @@
-def my_collect(array)
-  counter = 0
+#https://www.codecademy.com/en/forum_questions/51c72e759c4e9d410501df42
+#The puzzle here was to collect a new array without using collect, because each and collect have different return values
+#So you have to create a new array, that's all.
+def my_collect(array, first = 0)
+  counter = first
   myEmptyArray = []
   while counter < array.length
-    myEmptyArray << (yield(array[counter]))
-    counter += 1
+    myEmptyArray.push (yield(array[counter]))
+    counter = counter.next #.next is good for strings, man!
   end
   myEmptyArray
 end
