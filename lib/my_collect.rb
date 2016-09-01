c2df8b8b5aafeb43ccef535ef345a32f1bceece7
wcpines <colby@aptible.com>
2016-09-01 17:58:48 -0400

    gotta catch em all

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+# whatever you pass to the block has to be added to a *new array* object,
+# and that must be returned at the end
 
-
+collection = [1, 2, 3, 4]
+def my_collect(list)
+  i = 0
+  new_list = []
+  while i < list.length
+    new_list << (yield list[i])
+    i += 1
+  end
+  new_list
+end
