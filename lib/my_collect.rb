a6bfaca199b306580cbe8413bd7759f69937bbba
npatel111 <nitimaheshpatel@gmail.com>
2016-09-08 14:10:05 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,19 +1,5 @@
-names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-languages = ['ruby', 'javascript', 'python', 'objective-c']
-
-def my_collect(collection)
-  new_collection = []
-  i = 0
-  while i < collection.length
-    
-    new_collection << yield(collection[i])
-        i += 1
-
-  end  
-  new_collection
+def my_collect(array)
+  array.collect do |item|
+    yield(item)
+   end 
 end
-
-
-
-
-
